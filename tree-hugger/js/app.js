04b9dbe62bd6f249f175746ad9231ff7367ccd97
xming13 <xiaoming33@gmail.com>
2015-01-11 13:19:18 +0800

    Display lyrics when playing the song

--- tree-hugger/js/app.js
+++ tree-hugger/js/app.js
@@ -1,3 +1,71 @@
-/**
- * Created by ming on 1/10/15.
- */
+document.addEventListener("DOMContentLoaded", function () {
+
+    var pop = Popcorn("#audio");
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'audio/Antsy_Pants_-_Tree_Hugger.txt', true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var lrcArr = parseLrc(request.responseText);
+
+            var lrc, lrcNext;
+            for (var i = 0; i < lrcArr.length; i++) {
+                lrc = lrcArr[i];
+                lrcNext = lrcArr[i+1];
+
+                if (lrcNext) {
+                    pop.footnote({
+                        start: lrc.starttime,
+                        end: lrcNext.starttime,
+                        text: lrc.line,
+                        target: 'lyrics'
+                    });
+                }
+
+                pop.play();
+            }
+            console.log(lrcArr);
+        } else {
+            console.log('request reached server but error is returned.');
+        }
+    };
+
+    request.onerror = function() {
+        console.log('request.onerror');
+    };
+
+    request.send();
+
+    function parseLrc(rawLrc) {
+        var lrcArr = [];
+
+        var lrcAllRegex = /(\[[0-9.:\[\]]*\])+(.*)/;
+        var timeRegex = /\[([0-9]+):([0-9.]+)\]/;
+        var rawLrcArray = rawLrc.split(/[\r\n]/);
+
+        for (var i = 0; i < rawLrcArray.length; i++) {
+            // handle lrc
+            var lrc = lrcAllRegex.exec(rawLrcArray[i]);
+
+            if ( lrc && lrc[0] ) {
+                var times = lrc[1].replace(/\]\[/g,"],[").split(",");
+
+                for (var j = 0; j < times.length; j++) {
+                    var time = timeRegex.exec(times[j]);
+                    if ( time && time[0] ) {
+                        lrcArr.push( { "starttime": parseInt(time[1],10) * 60 + parseFloat(time[2]), "line": lrc[2] } );
+                    };
+                };
+            };
+        };
+
+        //sort lrcArr
+        lrcArr.sort(function (a,b) {
+            return a.starttime - b.starttime;
+        });
+
+        return lrcArr;
+    }
+
+}, false);

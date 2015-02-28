b699c60c80ac9d96d19ed931f6cc2353e9de37ff
xming13 <xiaoming33@gmail.com>
2015-02-28 22:20:25 +0800

    walking duckling

--- change/js/change.js
+++ change/js/change.js
@@ -5,16 +5,20 @@ $(document).ready(function () {
 
     timeline1.addLabel("start")
         .to(".sun", 2, {rotation: 90, transformOrigin: window.innerWidth / 2 + "px 50%"}, "start")
-        .to(".base", 3, { backgroundColor: "#00FFFF" }, "start")
+        .to(".base", 3, { backgroundColor: "#B0F0FF" }, "start")
 
     timeline1.addLabel("end", "+=3")
         .to(".sun", 2, { rotation: 180, transformOrigin: window.innerWidth / 2 + "px 50%" }, "end")
         .to(".base", 3, { backgroundColor: "#000" }, "end");
 
+    var stepDuckling = .25;
     duckTimeline.addLabel("duck")
         .to(".duckling", 10, { xPercent: "50%", right: "50%", ease: Linear.easeNone }, "duck")
-        .to(".duckling .head", .2, { x: "-=5", repeat: 49, yoyo: true }, "duck")
-        .to(".duckling .foot", .2, { rotation: 360, transformOrigin: "50% 50%", repeat: 49, yoyo: true }, "duck");
+        .to(".duckling .head", stepDuckling, { x: "-=5", repeat: 39, yoyo: true }, "duck")
+        .to(".duckling .foot-left-wrapper", stepDuckling, { rotation: -360, transformOrigin: "0 -10px", repeat: 19, repeatDelay: stepDuckling }, "duck")
+        .to(".duckling .foot-right-wrapper", stepDuckling, { rotation: -360, transformOrigin: "0 -10px", repeat: 19, repeatDelay: stepDuckling, delay: stepDuckling }, "duck")
+        .to(".duckling .foot.left", stepDuckling, { rotation: "+=360", repeat: 19, repeatDelay: stepDuckling }, "duck")
+        .to(".duckling .foot.right", stepDuckling, { rotation: "+=360", repeat: 19, repeatDelay: stepDuckling, delay: stepDuckling }, "duck");
 
     mainTimeline.add(timeline1, 1);
     mainTimeline.add(duckTimeline, 1);

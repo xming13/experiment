709b9a628a827b1985304bf5d77e04623cc14c91
xming13 <xiaoming33@gmail.com>
2015-03-01 12:11:56 +0800

    Improve animation

--- change/js/change.js
+++ change/js/change.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
     var mainTimeline = new TimelineMax({paused: true});
-    var timeline1 = new TimelineMax();
+    var backgroundTimeline = new TimelineMax();
     var duckTimeline = new TimelineMax();
     var caterpillarTimeline = new TimelineMax();
 
-    timeline1.addLabel("start")
+    backgroundTimeline.addLabel("start")
         .to(".sun", 2, {rotation: 90, transformOrigin: window.innerWidth / 2 + "px 50%"}, "start")
         .to(".base", 3, { backgroundColor: "#B0F0FF" }, "start")
 
-    timeline1.addLabel("end", "+=3")
+    backgroundTimeline.addLabel("end", "+=3")
         .to(".sun", 2, { rotation: 180, transformOrigin: window.innerWidth / 2 + "px 50%" }, "end")
         .to(".base", 3, { backgroundColor: "#000" }, "end");
 
     var durationDuckling = 10;
-    var delayDuckling = .5;
+    var delayDuckling = .25;
     var repeatDuckling = durationDuckling / delayDuckling / 2 - 1;
 
     duckTimeline.addLabel("duck")
@@ -24,20 +24,30 @@ $(document).ready(function () {
         .to(".duckling .foot.left", delayDuckling, { rotation: "-=360", repeat: repeatDuckling, repeatDelay: delayDuckling }, "duck")
         .to(".duckling .foot.right", delayDuckling, { rotation: "-=360", repeat: repeatDuckling, repeatDelay: delayDuckling, delay: delayDuckling }, "duck");
 
-    var durationCaterpillar = 10;
+    var durationCaterpillar = 15;
     var delayCaterpillar = .5;
-    var repeatCaterpillar = durationCaterpillar / delayCaterpillar / 2 - 1;
+    var repeatCaterpillar = durationCaterpillar / delayCaterpillar - 1;
 
-    caterpillarTimeline.addLabel("caterpillar")
-        .to(".caterpillar", durationCaterpillar, { xPercent: "-100%", left: "100%", ease: Linear.easeNone }, "caterpillar")
-        .to(".caterpillar .foot-left-wrapper", delayCaterpillar, { rotation: 360, transformOrigin: "5px -5px", repeat: repeatCaterpillar, repeatDelay: delayCaterpillar }, "caterpillar")
-        .to(".caterpillar .foot-right-wrapper", delayCaterpillar, { rotation: 360, transformOrigin: "5px -5px", repeat: repeatCaterpillar, repeatDelay: delayCaterpillar, delay: delayCaterpillar }, "caterpillar")
-        .to(".caterpillar .foot.left", delayCaterpillar, { rotation: "-=360", repeat: repeatCaterpillar, repeatDelay: delayCaterpillar }, "caterpillar")
-        .to(".caterpillar .foot.right", delayCaterpillar, { rotation: "-=360", repeat: repeatCaterpillar, repeatDelay: delayCaterpillar, delay: delayCaterpillar }, "caterpillar");    ;
+    var countCaterpillar = 0;
+    function tweenCaterpillar() {
 
-    mainTimeline.add(timeline1, 1);
+        // caterpillar is pushing forward
+        if (countCaterpillar % 2 == 0) {
+            TweenMax.to(".caterpillar", delayCaterpillar * 2, { left: "+=" + 100 * 2 / (repeatCaterpillar + 1) + "%" });
+        }
+        countCaterpillar++;
+    }
+
+    caterpillarTimeline
+        .to(".caterpillar .body1", delayCaterpillar, { x: "-=4", repeat: repeatCaterpillar, yoyo: true }, "caterpillar")
+        .to(".caterpillar .body2", delayCaterpillar, { x: "-=2", y: "-=4", repeat: repeatCaterpillar, yoyo: true }, "caterpillar")
+        .to(".caterpillar .body3", delayCaterpillar, { y: "-=10", repeat: repeatCaterpillar, yoyo: true, onRepeat: tweenCaterpillar }, "caterpillar")
+        .to(".caterpillar .body4", delayCaterpillar, { x: "+=2", y: "-=4", repeat: repeatCaterpillar, yoyo: true }, "caterpillar")
+        .to(".caterpillar .body5", delayCaterpillar, { x: "+=4", repeat: repeatCaterpillar, yoyo: true }, "caterpillar")
+
+    mainTimeline.add(backgroundTimeline, 1);
     mainTimeline.add(duckTimeline, 1);
-    mainTimeline.add(caterpillarTimeline, 5);
+    mainTimeline.add(caterpillarTimeline, 1);
 
     mainTimeline.play();
 });
